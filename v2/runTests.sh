56acaa06f39c996ea2af049dd71bc3d3751155b1
rodrigo-ta <miusc.tunes@gmail.com>
2020-11-16 14:40:01 +0000

    Final version of script

--- v2/runTests.sh
+++ v2/runTests.sh
@@ -9,8 +9,6 @@ do
     for ((thrds = 1; thrds <= $3; thrds++))
     do
     echo InputFile="$fl" NumThreads=$thrds
-    #./tecnicofs "$1/$fl" "$fl-$thrds.txt" $thrds | tee "$2/$fl-$thrds.txt" | grep 'TecnicoFS completed in'
-    ./tecnicofs "$1/$fl" "$fl-$thrds.txt" $thrds | grep 'TecnicoFS completed in'
-    mv "$fl-$thrds.txt" "$2"
+    ./tecnicofs "$1/$fl" "$2/${fl%.*}-$thrds.txt" $thrds | grep 'TecnicoFS completed in'
     done
 done
